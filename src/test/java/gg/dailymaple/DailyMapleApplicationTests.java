25770773f6d68cb19cdf5e1b02674a0e869209b1
Hyeon9mak <jinha3507@gmail.com>
2021-09-03 08:56:34 +0900

    chore: spring-boot initialize

---
+++ src/test/java/gg/dailymaple/DailyMapleApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.dailymaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMapleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
