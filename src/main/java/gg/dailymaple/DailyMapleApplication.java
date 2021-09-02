8b720548f5b52842c9aa12db1d66d376388b8094
Hyeon9mak <jinha3507@gmail.com>
2021-09-03 08:56:34 +0900

    chore: spring-boot initialize

---
+++ src/main/java/gg/dailymaple/DailyMapleApplication.java
@@ -0,0 +1,13 @@
+package gg.dailymaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMapleApplication.class, args);
+	}
+
+}
